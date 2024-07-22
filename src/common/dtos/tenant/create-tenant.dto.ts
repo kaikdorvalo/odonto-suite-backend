41073fd8baa900b4ef21cdc20ef2b5c2a488f8f7
Kaik KDS <kaikdorvalo@gmail.com>
2024-07-22 20:01:00 -0300

    created: create tenant dto

--- src/common/dtos/tenant/create-tenant.dto.ts
+++ src/common/dtos/tenant/create-tenant.dto.ts
@@ -1,8 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString, MinLength } from "class-validator";
 
 export class CreateTenantDto {
 
     @MinLength(3)
     @IsString()
+    @ApiProperty({
+        description: 'Tenant name'
+    })
     name: string;
 }

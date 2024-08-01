596769c306ae1641c6308a6174e68b3d14f2b269
Kaik KDS <kaikdorvalo@gmail.com>
2024-08-01 18:37:47 -0300

    updating: add master guard

--- src/modules/common/license/license.module.ts
+++ src/modules/common/license/license.module.ts
@@ -5,10 +5,12 @@ import { Repositories } from "../../../common/constants/respositories.constants"
 import { LicenseRepositoryImpl } from "./infrastructure/persistence/license.repository.impl";
 import { GenerateLicenseKeyUseCase } from "./application/use-case/generate-license-key/generate-license-key.use-case";
 import { DatabaseModule } from "../database/database.module";
+import { UserModule } from "src/modules/public/user/user.module";
 
 @Module({
     imports: [
-        DatabaseModule
+        DatabaseModule,
+        UserModule
     ],
     providers: [
         LicenseService,
--- src/modules/common/license/presentation/controllers/license.controller.ts
+++ src/modules/common/license/presentation/controllers/license.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Post, UseFilters, UseGuards } from "@nestjs/common";
 import { GenerateLicenseKeyUseCase } from "../../application/use-case/generate-license-key/generate-license-key.use-case";
+import { HttpExceptionFilter } from "../../../../../common/exception-filters/http-exception.filter";
+import { MasterGuard } from "../../../../../modules/common/auth/presentation/guards/master.guard";
 
 @Controller('licenses')
+@UseFilters(new HttpExceptionFilter())
 export class LicenseController {
     constructor(
         private readonly generateLicenseKeyUseCase: GenerateLicenseKeyUseCase
     ) { }
 
     @Post('generate')
+    @UseGuards(MasterGuard)
     async generateLicenseKey() {
         return await this.generateLicenseKeyUseCase.execute();
     }

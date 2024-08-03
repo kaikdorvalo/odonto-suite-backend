4a2c385bb37dc9ddc2511058df054e914a47d8e6
kaikdorvalo <kaikdorvalo@gmail.com>
2024-08-03 12:37:57 -0300

    migration: generate

---
+++ migrations/public/1722699452012-migration.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+export class Migration1722699452012 implements MigrationInterface {
+  name = 'Migration1722699452012'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const { schema } = queryRunner.connection.options as PostgresConnectionOptions;
+    
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const { schema } = queryRunner.connection.options as PostgresConnectionOptions;
+    
+  }
+}
